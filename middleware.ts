2d9ca5d76ed2e301789a651b4d576e6ca5c32418
Jannis Milz <jannis.milz@econ.uzh.ch>
2024-03-06 14:32:57 +0100

    Add middleware for authentication

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,71 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-import { supabase } from './lib/supabase'
- 
-export async function middleware(request: NextRequest) {
-  const { data: { session }, error } = await supabase.auth.getSession()
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { NextResponse, type NextRequest } from 'next/server'
 
-  if(!session || error) {
-    
+export async function middleware(request: NextRequest) {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  })
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "",
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          })
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+        },
+        remove(name: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value: '',
+            ...options,
+          })
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          })
+          response.cookies.set({
+            name,
+            value: '',
+            ...options,
+          })
+        },
+      },
+    }
+  )
+
+  const { data: { user }, error } = await supabase.auth.getUser()
+
+  if(!user || error) {
     if (request.nextUrl.pathname.startsWith('/admin')) {
-      return NextResponse.redirect(new URL('/admin/login', request.url))
+      return NextResponse.redirect(new URL('/admin/auth/login', request.url))
     }
     if (request.nextUrl.pathname.startsWith('/visits')) {
-      return NextResponse.redirect(new URL('/login', request.url))
+      return NextResponse.redirect(new URL('/auth/login', request.url))
     }
   }
+
+  return response
 }
  
 export const config = {
