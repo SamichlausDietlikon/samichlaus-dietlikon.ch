715d0d9bea5711ee8559d49412a207977ffb9906
Jannis Milz <jannis.milz@econ.uzh.ch>
2024-03-06 10:21:01 +0100

    Implement authorization middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,21 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { supabase } from './lib/supabase'
  
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  // Authentication logic
-  return NextResponse.redirect(new URL('/home', request.url))
+export async function middleware(request: NextRequest) {
+  const { data: { session }, error } = await supabase.auth.getSession()
+
+  if(!session || error) {
+    
+    if (request.nextUrl.pathname.startsWith('/admin')) {
+      return NextResponse.redirect(new URL('/admin/login', request.url))
+    }
+    if (request.nextUrl.pathname.startsWith('/visits')) {
+      return NextResponse.redirect(new URL('/login', request.url))
+    }
+  }
 }
  
-// See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/admin/:path*', '/auth/:path*', '/visits/:path*'],
+  matcher: ['/admin/((?!login))*', '/visits/:path*'],
 }
