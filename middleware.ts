dc935cf4ffdda65d7553f7b4e7313ccae6b366d4
Jannis Milz <jannis.milz@econ.uzh.ch>
2024-03-06 15:32:44 +0100

    Remove unnecessary code & add user conditional for staff & non-staff

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
+import { createClient } from './lib/supabase/server'
 
 export async function middleware(request: NextRequest) {
   let response = NextResponse.next({
@@ -8,66 +9,30 @@ export async function middleware(request: NextRequest) {
     },
   })
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "",
-    {
-      cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value,
-            ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          })
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          })
-        },
-        remove(name: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value: '',
-            ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          })
-          response.cookies.set({
-            name,
-            value: '',
-            ...options,
-          })
-        },
-      },
-    }
-  )
+  const supabase = createClient()
 
   const { data: { user }, error } = await supabase.auth.getUser()
 
+  
   if(!user || error) {
-    if (request.nextUrl.pathname.startsWith('/admin')) {
-      return NextResponse.redirect(new URL('/admin/auth/login', request.url))
-    }
-    if (request.nextUrl.pathname.startsWith('/visits')) {
       return NextResponse.redirect(new URL('/auth/login', request.url))
+  }
+  
+  const customUser = await supabase.from("users").select("staff_role").eq("id", user.id).limit(1).maybeSingle();
+
+  if (request.nextUrl.pathname.startsWith('/auth/login')) {
+    if(customUser && customUser.data?.staff_role) {
+      // TODO: If custom fields not filled out, redirect to setup form
+
+      return NextResponse.redirect(new URL('/admin', request.url))
     }
+
+    return NextResponse.redirect(new URL('/visits', request.url))
   }
 
   return response
 }
  
 export const config = {
-  matcher: ['/admin/((?!login))*', '/visits/:path*'],
+  matcher: ['/:path*'],
 }
