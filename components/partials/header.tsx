ba47d821960bc7cf87243f09d0e4611724b63a5c
Jannis Milz <jannis@milz.ch>
2024-03-11 23:07:43 +0100

    Hide header on admin setup route

--- components/partials/header.tsx
+++ components/partials/header.tsx
@@ -24,7 +24,7 @@ const components: { title: string; href: string}[] = [
 ]
 
 export default function Header() {
-  return (
+  return !window.location.pathname.startsWith("/admin/setup") && (
     <nav className="bg-white border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center h-16 w-full">
