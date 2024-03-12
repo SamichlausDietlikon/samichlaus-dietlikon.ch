e5cc52e42837c76f0940e7a182a1447e27b66214
Jannis Milz <jannis.milz@econ.uzh.ch>
2024-03-12 13:54:53 +0100

    After setup page everyone form the staff team should have a users row, so I can show the full name in the profile dropdown

--- components/partials/navigation/profile.tsx
+++ components/partials/navigation/profile.tsx
@@ -9,6 +9,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import useUser from "@/hooks/useUser";
+import { FullUser } from "@/types/common.types";
 import { ChevronDownIcon } from "@radix-ui/react-icons";
 
 export default function Profile() {
@@ -19,7 +20,7 @@ export default function Profile() {
   ) : (
     <DropdownMenu>
       <DropdownMenuTrigger className="flex items-baseline my-auto text-sm">
-        Mein Name 
+        {(user as FullUser).first_name} {(user as FullUser).last_name}
         <ChevronDownIcon
           className="relative top-[1px] ml-1 h-3 w-3 transition duration-300 group-data-[state=open]:rotate-180"
           aria-hidden="true"
