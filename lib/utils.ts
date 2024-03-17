94975d1e09c0b62a4098721e85e12d8e0c0db472
Jannis Milz <jannis@milz.ch>
2024-03-17 21:21:47 +0100

    Add staff roles name formatting object

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const staffRoles = {
+  "admin": "Admin",
+  "tour_manager": "Touren Management",
+  "staff": "Mitglied",
+}
