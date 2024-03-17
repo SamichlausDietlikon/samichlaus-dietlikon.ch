dc923df956eaf7e1084b6c8a3abb5e92bf2425a2
Jannis Milz <jannis@milz.ch>
2024-03-17 21:22:15 +0100

    Only pick email type from auth user

--- types/common.types.ts
+++ types/common.types.ts
@@ -1,4 +1,4 @@
 import { User } from "@supabase/supabase-js";
 import { Tables } from "./database.types";
 
-export type FullUser = User & Tables<"users"> & Tables<"user_staff_roles">
+export type FullUser = Pick<User,"email"> & Tables<"users"> & Tables<"user_staff_roles">
