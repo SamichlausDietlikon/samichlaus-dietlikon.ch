09ba901c0ab652a3a33c1b42bd91ad405c7851a9
Jannis Milz <jannis@milz.ch>
2024-03-19 09:22:11 +0100

    Adjust button sizes

--- app/(secured)/admin/templates/[templateId]/page.tsx
+++ app/(secured)/admin/templates/[templateId]/page.tsx
@@ -7,9 +7,16 @@ import Link from "next/link";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import useUser from "@/hooks/useUser";
+import { FullUser } from "@/types/common.types";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 
 export default function SingleTemplate() {
   const { templateId } = useParams()
+
+  const {loading, user} = useUser()
 
   const [template, setTemplate] = useState<null|Tables<"tour_templates"> & { tour_template_versions: Tables<"tour_template_versions">[] }>(null)
 
@@ -36,6 +43,41 @@ export default function SingleTemplate() {
       <p className="text-xl">Beschreibung:</p>
       <p className="text-sm my-1 text-stone-500">{template.description}</p>
       <h2 className="text-xl my-8">Versionen</h2>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Version</TableHead>
+            <TableHead>Beschreibung</TableHead>
+            <TableHead>Tag</TableHead>
+            <TableHead className="text-right">
+              {/* {(user as FullUser).staff_role === "admin" && (
+                <TemplateFormDialog refetch={refetch} setRefetch={setRefetch} />
+              )} */}
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {template.tour_template_versions.map((version) => (
+            <TableRow key={version.id}>
+              <TableCell><Badge variant="default">v{version.version}</Badge></TableCell>
+              <TableCell className="max-w-[28ch] text-nowrap overflow-hidden overflow-ellipsis">{version.description ?? "-"}</TableCell>
+              <TableCell><Badge variant={version.tag}>{version.tag}</Badge></TableCell>
+              <TableCell className="text-right">
+                {(user as FullUser).staff_role === "admin" && (
+                  <>
+                    <Button variant="link" className="text-red-400 hover:text-red-500" >
+                      Löschen
+                    </Button>
+                  </>
+                )}
+                <Link href={`/admin/versions/${template.id}`} className="text-primary underline-offset-4 hover:underline text-sm font-medium h-9 px-2 py-2">
+                  Anzeigen
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </section>
   )
 }
