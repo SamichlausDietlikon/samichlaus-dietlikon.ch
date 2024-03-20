4045d95609501b4faa5af686507d7dfd2b43a163
Jannis Milz <jannis@milz.ch>
2024-03-20 16:44:09 +0100

    Add seasons layout for sidebar

--- app/(secured)/admin/seasons/layout.tsx
+++ app/(secured)/admin/seasons/layout.tsx
@@ -3,13 +3,15 @@
 import Sidebar from "@/components/partials/sidebar"
 import useUser from "@/hooks/useUser"
 
-export default function Seasons() {
+export default function SeasonLayout({children}: {children: React.ReactNode}) {
   const {loading, user} = useUser()
  
   return loading ? (
     <div>Loading...</div>
   ) : (
-    <section className="h-full"> 
-    </section>
+    <div className="h-full flex"> 
+      <Sidebar />
+      {children}
+    </div>
   )
 }
