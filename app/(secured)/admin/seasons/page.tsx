f3b5fcdd605741a3a04257dd66c497bee58c61ed
Jannis Milz <jannis@milz.ch>
2024-03-20 08:43:37 +0100

    Only render sidebar after user loading finished

--- app/(secured)/admin/seasons/page.tsx
+++ app/(secured)/admin/seasons/page.tsx
@@ -1,9 +1,14 @@
 "use client"
 
 import Sidebar from "@/components/partials/sidebar"
+import useUser from "@/hooks/useUser"
 
 export default function Seasons() {
-  return (
+  const {loading, user} = useUser()
+ 
+  return loading ? (
+    <div>Loading...</div>
+  ) : (
     <section className="h-full"> 
       <Sidebar />
     </section>
