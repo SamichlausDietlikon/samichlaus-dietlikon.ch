71d7c881cc8a5e8b8c9e0ef9fc592dbc9c97d92d
Jannis Milz <jannis@milz.ch>
2024-03-21 21:19:17 +0100

    Fix season members page

--- app/(secured)/admin/seasons/[seasonId]/members/page.tsx
+++ app/(secured)/admin/seasons/[seasonId]/members/page.tsx
@@ -17,30 +17,30 @@ import { Tables } from "@/types/database.types";
 import { seasonStaffRoles } from "@/lib/utils";
 import MembersFormDialog from "@/components/members/members-form-dialog";
 
-export default function Members() {
-  const [members, setMembers] = useState<[]|(Tables<"season_members"> & { users: Tables<"users">[] })[]>([])
+export default function Members({params}: {params: {seasonId: string}}) {
+  const [members, setMembers] = useState<[]|(Tables<"season_members"> & { users: Tables<"users">|null })[]>([])
   const [refetch, setRefetch] = useState<boolean>(false)
-
+  
   const { loading, user } = useUser();
-
+  
   const supabase = createClient()
   
   useEffect(() => {
-    supabase.from("season_members").select("*, users!innner(*)").then(({ data, error }) => {
+    supabase.from("season_members").select("*, users!inner(*)").eq("season_id", params.seasonId).then(({ data, error }) => {
       if (error) {
         console.error(error)
         return
       }
-
+      
       setMembers(data)
     })
   }, [refetch, supabase])
-
-
+  
+  
   return loading ? (
     <div>Loading...</div>
-  ) : (
-    <section className="max-w-7xl w-full px-8">
+    ) : (
+      <section className="max-w-7xl w-full px-8">
       <h1 className="text-2xl my-8">Saison Mitglieder</h1>
       <Table className="w-full">
         <TableHeader>
@@ -58,13 +58,13 @@ export default function Members() {
         <TableBody>
           {members?.map((member) => (
             <TableRow key={member.id}>
-              <TableCell>{member.users[0].first_name ?? "-"}</TableCell>
-              <TableCell>{member.users[0].last_name ?? "-"}</TableCell>
+              <TableCell>{member.users?.first_name ?? "-"}</TableCell>
+              <TableCell>{member.users?.last_name ?? "-"}</TableCell>
               <TableCell>{seasonStaffRoles[member.staff_role]}</TableCell>
               <TableCell className="text-right">
-                {/* {(user as FullUser).staff_role === "admin" && member.users[0].id !== user?.id && (
-                  <UsersFormDialog user={member} refetch={refetch} setRefetch={setRefetch} />
-                )} */}
+                {(user as FullUser).staff_role === "admin" && member.users?.id !== user?.id && (
+                  <MembersFormDialog member={member} refetch={refetch} setRefetch={setRefetch} />
+                )}
               </TableCell>
             </TableRow>
           ))}
