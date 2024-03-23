026aee7c388e337feb69b17ad37f1f3d7598a0bb
Jannis Milz <jannis@milz.ch>
2024-03-24 00:08:06 +0100

    Add FAQ (or called informations)

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -10,6 +10,114 @@ import {
 import Image from "next/image";
 import Autoplay from "embla-carousel-autoplay";
 import { useRef } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import Link from "next/link";
+
+// Wrap answer in <>...</> to allow valid JSX
+const faq = [
+  {
+    question: "Wie bekomme ich einen Chlausbesuch?",
+    answer: (
+      <>
+        Direkt auf unserer Homepage unter{" "}
+        <Link href="/apply/family" className="base-link">
+          Anmeldung Familienbesuche
+        </Link>{" "}
+        und{" "}
+        <Link href="/apply/business" className="base-link">
+          Anmeldung Schulen / Firmen
+        </Link>
+        .
+        <br /> Bei Fragen und Schwierigkeiten wenden Sie sich bitte direkt an uns, über
+        unser{" "}
+        <Link href="/contact" className="base-link">
+          Kontaktformular
+        </Link>
+        .
+      </>
+    ),
+  },
+  {
+    question: "Was kostet ein Besuch beim Chlaus?",
+    answer: (
+      <>
+        <p>
+          Liebe Familie, dies ist Ihr freies Ermessen. Der Samichlaus und der Schmutzli
+          machen alle Privatbesuche auf freiwilliger Basis und verlangen deshalb keinen
+          festen Preis. Selbstverständlich muss auch der Samichlaus seine Unkosten decken
+          und ist deshalb auf freiwillige Spenden angewiesen. Der Esel braucht Futter, das
+          Gewand muss geflickt und die Stiefel besohlt werden.
+        </p>
+
+        <p>
+          Am einfachsten erfolgt die Spende direkt am Ende des Besuches. Geben Sie beim
+          Verabschieden des Samichlaus oder des Schmutzli ein Nötli oder ein Couvert mit
+          Inhalt nach eigenem Gutdünken. Sie können es ihnen diskret direkt in die Finger
+          drücken.
+        </p>
+      </>
+    ),
+  },
+  {
+    question: "Wie pünktlich ist der Chlaus?",
+    answer:
+      "Der Chlaus trägt keine Uhr, trotzdem schafft er es meistens die Besuchszeit ziemlich genau einzuhalten. Bitte haben Sie Verständnis, dass der Chlaus während dem Besuch 100%ig für die besuchten Kinder da ist und ein Besuch auch einmal länger als geplant werden kann. Das kann auch bei Ihrem Kind / Ihren Kindern der Fall sein. Auch schlechte Strassen- und Wetterverhältnissen können den Chlaus daran hindern, den Zeitplan einzuhalten. Sollten wir uns viel verspäten, informieren wir Sie nach Möglichkeit telefonisch.",
+  },
+  {
+    question: "Wie ist der Ablauf beim Chlausbesuch in der Waldhütte",
+    answer: (
+      <>
+        <p>
+          Der Samichlaus und der Schmutzli warten in der{" "}
+          <Link href="https://maps.app.goo.gl/BGBcUnY3sxrFvwWy7" className="base-link">
+            Waldhütte 4
+          </Link>{" "}
+          im Hardwald auf Sie und Ihre Familie.
+        </p>
+
+        <p>
+          Falls Sie mit dem Auto kommen, fahren Sie vis à vis des Loorenhofs in den Wald
+          und parken Ihr Auto beim Seewadel. Von dort aus gehen Sie bitte zu Fuss bis zur
+          Waldhütte. Der Weg ist gekennzeichnet. Folgen Sie dem Chlietürliweg, welcher
+          parallel zur Hauptstrasse verläuft, bis zur nächsten Weggabelung. Dort finden
+          Sie den beleuchteten Herrenbänkliweg, welchem Sie bis zur Waldhütte folgen, wo
+          der Samichlaus mit dem Schmutzli auf Sie wartet.
+        </p>
+
+        <p>
+          Vor der Waldhütte werden Sie von Helfern in Empfang genommen, die um einen
+          reibungslosen Ablauf bemüht sind.
+        </p>
+        <p>
+          Die Säckchen für die Kinder bringen Sie am besten in einem unauffälligen Sack
+          mit. Diesen können Sie den Helfern vor Ort übergeben. Sie werden sich um alles
+          weitere kümmern. Auch die Bezahlung können sie vor Ort mit den Helfern
+          abwickeln.
+        </p>
+        <p>
+          Bitte erscheinen Sie möglichst genau auf ihre Buchungszeit, damit es weder Stau
+          noch Verzögerungen gibt.
+        </p>
+        <p>
+          Falls Sie Fragen haben, melden Sie sich bitte telefonisch unter{" "}
+          <Link href="tel:+41794668505" className="base-link">
+            079 466 85 05
+          </Link>
+          .
+        </p>
+      </>
+    ),
+  },
+  {
+    question: "Wie werde ich Passivmitglied?",
+    answer: "Yes. It's animated by default, but you can disable it if you prefer.",
+  },
+];
 
 export default function Home() {
   const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
@@ -53,6 +161,10 @@ export default function Home() {
             voller Vorfreude für die kommende Adventszeit.
           </p>
           <p>Bis bald ...</p>
+        </div>
+      </div>
+      <div>
+        <div className="max-w-7xl m-auto my-12 px-8 space-y-4 [&>p]:max-w-4xl [&>p]:m-auto">
           <h2 className="text-2xl text-center !mt-12">Legende</h2>
           <p>
             <em>
@@ -110,6 +222,19 @@ export default function Home() {
           <p>
             <em>Die Menschen waren glücklich und jubelten dem Bischof Niklaus zu.</em>
           </p>
+        </div>
+      </div>
+      <div>
+        <div className="max-w-4xl m-auto my-12 px-8 space-y-4 [&>p]:max-w-4xl [&>p]:m-auto">
+          <h2 className="text-2xl text-center !mt-12">Informationen</h2>
+          <Accordion type="single" collapsible className="w-full">
+            {faq.map(({ question, answer }, index) => (
+              <AccordionItem value={`question-${index}`}>
+                <AccordionTrigger>{question}</AccordionTrigger>
+                <AccordionContent className="space-y-4">{answer}</AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
         </div>
       </div>
     </section>
