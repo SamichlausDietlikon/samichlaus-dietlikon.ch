d48407f00a5946f50b56b543ead0a7ef305a9910
Jannis Milz <jannis@milz.ch>
2024-03-23 23:10:17 +0100

    Add landing page image carousel

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,3 +1,46 @@
+"use client";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+import Autoplay from "embla-carousel-autoplay";
+import { useRef } from "react";
+
 export default function Home() {
-  return <div>Hello Public</div>;
+  const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
+
+  return (
+    <div>
+      <Carousel
+        plugins={[plugin.current]}
+        opts={{
+          align: "start",
+        }}
+        className="w-[960px] m-auto mt-12 rounded-lg"
+        onMouseEnter={plugin.current.stop}
+        onMouseLeave={plugin.current.reset}
+      >
+        <CarouselContent>
+          {[...Array(6).keys()].map((i) => (
+            <CarouselItem>
+              <Image
+                src={`/images/${i + 1}.jpg`}
+                alt={`Samichlaus Dietlikon Bild ${i + 1}`}
+                width={960}
+                height={400}
+                className="rounded-lg"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
 }
